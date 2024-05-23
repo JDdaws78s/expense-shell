e298d04d3b8b30a6b4d7bc9d367de94e5d4d557d
suresh <sureshkmr123789@gmail.com>
2024-05-23 15:53:33 +0530

    shell practice

--- backend_shell.sh
+++ backend_shell.sh
@@ -79,7 +79,7 @@ VALIDATE $? "enable backend"
 dnf install mysql -y &>>$LOGFILE
 VALIDATE $? "MYSQL installation"
 
-mysql -h db.sureshm.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOGFILE
+mysql -h db.sureshm.online -uroot -p{db_root_password} < /app/schema/backend.sql &>>$LOGFILE
 VALIDATE $? "load schema"
 
 systemctl restart backend &>>$LOGFILE
